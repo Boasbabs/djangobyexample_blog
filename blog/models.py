4f218d9a38977435dab4e109785577c19ce4e084
Boasbabs <babalolasimeon@gmail.com>
2018-03-21 11:44:08 +0000

    Add views and urls for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -19,8 +25,20 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default="draft")
 
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Our custom manager
+
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[
+                           self.publish.year,
+                           self.publish.strftime("%m"),
+                           self.publish.strftime("%d"),
+                           self.slug
+                       ])
 
     def __str__(self):
         return self.title
